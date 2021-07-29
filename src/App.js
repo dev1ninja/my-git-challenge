9c3f8c5b9e7a3b125e268437682a67cab0729eaa
dev1ninja <devninja.up@gmail.com>
2021-07-29 12:10:58 +0900

    Upwork interview

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import BiologyToday from './components/biologyToday'
+import AutoReview from './components/autoReview'
+
+function App() {
+  return (
+    <div className="App">
+      <img className="coolLogo" src="https://images.unsplash.com/photo-1618477388954-7852f32655ec?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1300&q=80" alt="my really cool logo" />
+      <header className="App-header">
+        <p>
+          My really cool blog site
+        </p>
+      </header>
+      <BiologyToday/>
+      <AutoReview/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders My really cool blog site logo', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/My really cool blog site/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Biology Today article', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Biology Today:/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Auto Review Article', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Auto Review:/i);
+  expect(linkElement).toBeInTheDocument();
+});
